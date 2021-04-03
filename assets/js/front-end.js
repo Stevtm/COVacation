9ac5453732f76b71f6d1955c50c0e2e66ef5dd88
Stevtm <steventmaddison@outlook.com>
2021-04-03 10:18:43 -0400

    style previous searches section

---
+++ assets/js/front-end.js
@@ -0,0 +1,9 @@
+// ----- functions that change the front-end styling -----
+
+// function that reduces the size of the header once the "submit" button is first clicked
+var shrinkHeader = function () {};
+
+// ----- event listeners -----
+$("#search-submit").on("click", function () {
+	console.log("search button clicked");
+});
