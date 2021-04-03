088a0fa4c70d6b1c08a6d99ceacffef6587b6505
Stevtm <steventmaddison@outlook.com>
2021-04-03 10:54:42 -0400

    resize header on submit click

--- assets/js/front-end.js
+++ assets/js/front-end.js
@@ -1,9 +1,39 @@
 // ----- functions that change the front-end styling -----
 
 // function that reduces the size of the header once the "submit" button is first clicked
-var shrinkHeader = function () {};
+var shrinkHeader = function () {
+	console.log("time to shrink the header!");
+
+	// resize the site name and put it in the corner
+	$("h1")
+		.removeClass("pt-12 text-center text-5xl")
+		.addClass("text-2xl text-left");
+
+	// remove the slogan from the page
+	$("header").find("p").remove();
+
+	// reduce the height of the header so that it sits at the top of the page
+	$("header").addClass("transition-all");
+	$("header").removeClass("p-4 pt-40").addClass("p-2");
+
+	// slightly reduce the size of the form elements
+	$("form").find("span.p-4").removeClass("p-4").addClass("p-3");
+	$("form").find("input.p-2").removeClass("p-2").addClass("p-1 px-2");
+	$("form").find("span.p-1").removeClass("p-1");
+};
+
+// function that displays the destination image and covid information
+var showCOVIDInfo = function () {};
 
 // ----- event listeners -----
-$("#search-submit").on("click", function () {
-	console.log("search button clicked");
+
+// listen for the click on the search button
+$("#search-submit").on("click", function (event) {
+	// stop the page from reloading
+	event.preventDefault();
+
+	// change the header configuration to the smaller version
+	shrinkHeader();
+
+	// display the destination image and covid information
 });
