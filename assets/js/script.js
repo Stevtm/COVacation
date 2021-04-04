d9c0c011b58c124e6547df4d61a7c894001136fe
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-04-04 19:17:49 -0400

    commented non-required code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,48 +3,48 @@ $("#search-submit").on("click", showFlightInfo);
 
 
 
-// function to get a list of airports at both origin and destination (separate calls for each)
-function getPlaces(queryName) {
-    fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/CA/CAD/en-US/?query=${queryName}`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "e94756adcfmsh4384f35646a8fc5p17f530jsn906fc3f8b7a4",
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-		}
-	})
-	.then(response => {
-		response.json().then(data => {
-		console.log(data);
-		console.log(data.Places[1].PlaceId); // gets most convenient airport
+// // function to get a list of airports at both origin and destination (separate calls for each)
+// function getPlaces(queryName) {
+//     fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/CA/CAD/en-US/?query=${queryName}`, {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "e94756adcfmsh4384f35646a8fc5p17f530jsn906fc3f8b7a4",
+// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+// 		}
+// 	})
+// 	.then(response => {
+// 		response.json().then(data => {
+// 		console.log(data);
+// 		console.log(data.Places[1].PlaceId); // gets most convenient airport
 
 		
-		});
-	})
-	.catch(err => {
-		console.error(err);
-	});
+// 		});
+// 	})
+// 	.catch(err => {
+// 		console.error(err);
+// 	});
 
-}
+// }
 
-function showFlights() {
-	fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/CA/CAD/en-US/YYZ-sky/CLE-sky/anytime?inboundpartialdate=anytime", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "e94756adcfmsh4384f35646a8fc5p17f530jsn906fc3f8b7a4",
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-		}
-	})
-	.then(response => {
-		console.log(response);
-		response.json().then(data => {
-			console.log(data);
-		})
-	})
-	.catch(err => {
-		console.error(err);
-	});
+// function showFlights() {
+// 	fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/CA/CAD/en-US/YYZ-sky/CLE-sky/anytime?inboundpartialdate=anytime", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "e94756adcfmsh4384f35646a8fc5p17f530jsn906fc3f8b7a4",
+// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+// 		}
+// 	})
+// 	.then(response => {
+// 		console.log(response);
+// 		response.json().then(data => {
+// 			console.log(data);
+// 		})
+// 	})
+// 	.catch(err => {
+// 		console.error(err);
+// 	});
 
-}
+// }
 
 function showFlightInfo() {
 	var departFrom = $("#depart-from").val().trim();
@@ -117,13 +117,11 @@ function showFlightInfo() {
 										var cityLon = data.results[0].geometry.lng;
 										var searchCountry = data.results[0].components.country;
 										var threeLetterSymbol = data.results[0].components["ISO_3166-1_alpha-3"].toLowerCase();
-										console.log(searchCountry +"	"+ threeLetterSymbol +"		"+ cityLat +"		"+ cityLon);
+										console.log(`Destination: ${searchCountry}, ThreeLetterSymbol: ${threeLetterSymbol}, Latitude: ${cityLat}, Longitude: ${cityLon}`);
 
 										if(searchCountry === "United States"){
 											searchCountry = "USA";
 										}
-
-										console.log(`Search Country: ${searchCountry}`);
 
 										// fetch covid info
 										fetch(`https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/country-report-iso-based/${searchCountry}/${threeLetterSymbol}`, {
@@ -137,7 +135,7 @@ function showFlightInfo() {
 											response.json()
 											.then(covidData => {
 												console.log(covidData[0]); // get covid data from this object
-												console.log(`New Cases: ${covidData[0].NewCases}	Total Cases: ${covidData[0].TotalCases}		Active cases: ${covidData[0].ActiveCases}		Recovery Proportion: ${covidData[0].Recovery_Proporation}`)
+												console.log(`New Cases: ${covidData[0].NewCases}	Total Cases: ${covidData[0].TotalCases}		Active cases: ${covidData[0].ActiveCases}		Recovery Proportion: ${covidData[0].Recovery_Proporation}%`)
 
 												fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly,alerts&appid=3e8aa64128a8b382a871af127be1e2d0&units=imperial`)
 												.then(response => {
@@ -179,66 +177,66 @@ function showFlightInfo() {
 // use this function to call covid and weather api's
 
 
-function infoLookup(cityName) {
+// function infoLookup(cityName) {
 
 
-	fetch(`https://api.opencagedata.com/geocode/v1/json?key=bf9cc7921eff4de78bd861e487142a0a&pretty=1&q=${cityName}&no_annotations=1`)
-	.then(response => {
-		if(response.ok){
-			response.json()
-			.then(data => {
-				console.log(data); // data from opencage api
+// 	fetch(`https://api.opencagedata.com/geocode/v1/json?key=bf9cc7921eff4de78bd861e487142a0a&pretty=1&q=${cityName}&no_annotations=1`)
+// 	.then(response => {
+// 		if(response.ok){
+// 			response.json()
+// 			.then(data => {
+// 				console.log(data); // data from opencage api
 
-				var cityLat = data.results[0].geometry.lat;
-				var cityLon = data.results[0].geometry.lng;
-				var searchCountry = data.results[0].components.country;
-				var threeLetterSymbol = data.results[0].components["ISO_3166-1_alpha-3"].toLowerCase();
-				console.log(searchCountry +"	"+ threeLetterSymbol +"		"+ cityLat +"		"+ cityLon);
+// 				var cityLat = data.results[0].geometry.lat;
+// 				var cityLon = data.results[0].geometry.lng;
+// 				var searchCountry = data.results[0].components.country;
+// 				var threeLetterSymbol = data.results[0].components["ISO_3166-1_alpha-3"].toLowerCase();
+// 				console.log(searchCountry +"	"+ threeLetterSymbol +"		"+ cityLat +"		"+ cityLon);
 
-				if(searchCountry === "United States"){
-					searchCountry = "USA";
-				}
+// 				if(searchCountry === "United States"){
+// 					searchCountry = "USA";
+// 				}
 
-				// fetch covid info
-				fetch(`https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/country-report-iso-based/${searchCountry}/${threeLetterSymbol}`, {
-					"method": "GET",
-					"headers": {
-						"x-rapidapi-key": "e94756adcfmsh4384f35646a8fc5p17f530jsn906fc3f8b7a4",
-						"x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
-					} 
-				})
-				.then(response => {
-					response.json()
-					.then(covidData => {
-						console.log(covidData); // get covid data from this object
+// 				// fetch covid info
+// 				fetch(`https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/country-report-iso-based/${searchCountry}/${threeLetterSymbol}`, {
+// 					"method": "GET",
+// 					"headers": {
+// 						"x-rapidapi-key": "e94756adcfmsh4384f35646a8fc5p17f530jsn906fc3f8b7a4",
+// 						"x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+// 					} 
+// 				})
+// 				.then(response => {
+// 					response.json()
+// 					.then(covidData => {
+// 						console.log(covidData); // get covid data from this object
 
 
 						
-						fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly,alerts&appid=3e8aa64128a8b382a871af127be1e2d0&units=imperial`)
-						.then(response => {
-							response.json()
-							.then(weatherData => {
-								console.log(weatherData);
-							});
-						});
+// 						fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly,alerts&appid=3e8aa64128a8b382a871af127be1e2d0&units=imperial`)
+// 						.then(response => {
+// 							response.json()
+// 							.then(weatherData => {
+// 								console.log(weatherData);
+// 							});
+// 						});
 
 						
 
-					})
-				});
+// 					})
+// 				});
 
 				
 				
-			})
-		}
-		else {
-			console.log("Error with response"); // add modal here for showing response on screen.
-		}
-	})
+// 			})
+// 		}
+// 		else {
+// 			console.log("Error with response"); // add modal here for showing response on screen.
+// 		}
+// 	})
 
 
 
-}
+// }
 
 
 /* To-Do
