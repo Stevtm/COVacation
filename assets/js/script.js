c74b0643206b1b8e8cf6b095822c5bd80f4f45a7
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-04-04 13:13:36 -0400

    setting up file structure for skyscanner api

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,9 @@ function getPlaces(queryName) {
 	.then(response => {
 		response.json().then(data => {
 		console.log(data);
-		console.log(data.Places);
+		console.log(data.Places[1].PlaceId); // gets most convenient airport
+
+		
 		});
 	})
 	.catch(err => {
@@ -119,22 +121,21 @@ function showFlights() {
 // using opencage geocoding api.  https://opencagedata.com/api#request
 // fix covid api function to use this before fetch request
 // use this function to call covid and weather api's
+
+
 function infoLookup(cityName) {
-	fetch(`https://api.opencagedata.com/geocode/v1/json?key=bf9cc7921eff4de78bd861e487142a0a&pretty=1&q=${cityName}`)
+	fetch(`https://api.opencagedata.com/geocode/v1/json?key=bf9cc7921eff4de78bd861e487142a0a&pretty=1&q=${cityName}&no_annotations=1`)
 	.then(response => {
 		if(response.ok){
 			response.json()
 			.then(data => {
-				// console.log(`${cityName}		${data.results[0].components.country}		${data.results[0].components['ISO_3166-1_alpha-3']}`);
-				// showCovidInfo()
+				console.log(data); // data from opencage api
 
-				// console.log(data); // data from opencage api
-
-				var cityLat = data.results[0].annotations.DMS.lat;
-				var cityLon = data.results[0].annotations.DMS.lng;
+				var cityLat = data.results[0].geometry.lat;
+				var cityLon = data.results[0].geometry.lng;
 				var searchCountry = data.results[0].components.country;
 				var threeLetterSymbol = data.results[0].components["ISO_3166-1_alpha-3"].toLowerCase();
-				console.log(searchCountry , threeLetterSymbol , cityLat , cityLon);
+				console.log(searchCountry +"	"+ threeLetterSymbol +"		"+ cityLat +"		"+ cityLon);
 
 				// fetch covid info
 				fetch(`https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/country-report-iso-based/${searchCountry}/${threeLetterSymbol}`, {
@@ -147,18 +148,20 @@ function infoLookup(cityName) {
 				.then(response => {
 					response.json()
 					.then(covidData => {
-						// console.log(covidData[0]); // get covid data from this object
+						console.log(covidData[0]); // get covid data from this object
 
 
-						// get weather info from this fetch
-						// use onecall api instead with lat lon from opencage api data
-						fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=3e8aa64128a8b382a871af127be1e2d0&units=imperial&exclude=current,minutely,hourly`)
+						
+						fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly,alerts&appid=3e8aa64128a8b382a871af127be1e2d0&units=imperial`)
 						.then(response => {
 							response.json()
 							.then(weatherData => {
 								console.log(weatherData);
-							})
-						})
+							});
+						});
+
+						
+
 					})
 				});
 
@@ -171,6 +174,7 @@ function infoLookup(cityName) {
 		}
 	})
 }
+
 
 /* To-Do
 	fetch flights and weather info using data from infoLookup
