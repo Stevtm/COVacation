51b0f1ada4d16ebfb86692abdde37a4c7db45481
Stevtm <steventmaddison@outlook.com>
2021-04-03 20:05:27 -0400

    style flight search results

--- assets/js/front-end.js
+++ assets/js/front-end.js
@@ -52,7 +52,7 @@ var showCOVIDInfo = function () {
 	var newCasesEl = $("<div>").addClass("p-2 text-center w-6/12");
 
 	var newCasesTitle = $("<h4>").addClass("text-red-600").text("New Cases");
-	var newCasesVal = $("<p>").addClass("font-semibold text-3xl").text("6,098");
+	var newCasesVal = $("<p>").addClass("font-semibold text-2xl").text("6,098");
 
 	newCasesEl.append(newCasesTitle, newCasesVal);
 
@@ -61,7 +61,7 @@ var showCOVIDInfo = function () {
 
 	var totalCasesTitle = $("<h4>").addClass("text-red-600").text("Total Cases");
 	var totalCasesVal = $("<p>")
-		.addClass("font-semibold text-3xl")
+		.addClass("font-semibold text-2xl")
 		.text("996,702");
 
 	totalCasesEl.append(totalCasesTitle, totalCasesVal);
@@ -70,7 +70,7 @@ var showCOVIDInfo = function () {
 	var newVaxEl = $("<div>").addClass("p-2 text-center w-6/12");
 
 	var newVaxTitle = $("<h4>").addClass("text-red-600").text("New Vaccinations");
-	var newVaxVal = $("<p>").addClass("font-semibold text-3xl").text("126,257");
+	var newVaxVal = $("<p>").addClass("font-semibold text-2xl").text("126,257");
 
 	newVaxEl.append(newVaxTitle, newVaxVal);
 
@@ -81,7 +81,7 @@ var showCOVIDInfo = function () {
 		.addClass("text-red-600")
 		.text("Total Vaccinations");
 	var totalVaxVal = $("<p>")
-		.addClass("font-semibold text-3xl")
+		.addClass("font-semibold text-2xl")
 		.text("8,468,570");
 
 	totalVaxEl.append(totalVaxTitle, totalVaxVal);
@@ -105,6 +105,121 @@ var showCOVIDInfo = function () {
 	$("main").prepend(sectionEL);
 };
 
+// function that shows flights matching the criteria
+// details argument must include: to/from locations and depart/return date
+var showFlights = function (details) {
+	// declare array of flight details objects
+	var flightsArray = [
+		{
+			departLeave: "7:00 AM",
+			departArrive: "9:00 AM",
+			returnLeave: "7:30 AM",
+			returnArrive: "3:00 PM",
+			price: 470,
+		},
+		{
+			departLeave: "9:00 AM",
+			departArrive: "11:00 AM",
+			returnLeave: "11:00 AM",
+			returnArrive: "6:30 PM",
+			price: 500,
+		},
+		{
+			departLeave: "11:00 AM",
+			departArrive: "1:00 PM",
+			returnLeave: "1:00 PM",
+			returnArrive: "8:30 PM",
+			price: 350,
+		},
+	];
+
+	console.log("time to show some flights!");
+	// create a section element to hold all of the flight results
+	var flightsEl = $("<section>").addClass(
+		"m-3 border border-gray-400 rounded-xl shadow-md w-10/12"
+	);
+
+	// create a title for the section
+	var flightsLocation = $("<h2>")
+		.addClass("pt-4 rounded-t-xl text-3xl font-semibold text-center bg-red-100")
+		.text("Trips from Toronto to Vancouver");
+	var flightsDate = $("<h3>")
+		.addClass(
+			"pb-4 text-2xl text-center font-semibold text-gray-700 bg-red-100"
+		)
+		.text("Sun, Apr 18 to Wed, Apr 21");
+
+	// append to the flightsEl
+	flightsEl.append(flightsLocation, flightsDate);
+
+	// create a row for each search result (need to decide how many we will show)
+	for (trip of flightsArray) {
+		// create a div to hold the flight information
+		tripEl = $("<div>").addClass(
+			"trip py-2 flex flex-row border-t border-gray-400 cursor-pointer hover:bg-gray-100 w-full"
+		);
+
+		detailsEl = $("<div>").addClass("flex flex-col w-8/12");
+
+		// create a div and children for the departure flight times
+		departEl = $("<div>").addClass("flex flex-row w-full");
+		departIcon = $("<span>")
+			.addClass(
+				"p-3 flex flex-col justify-center items-center text-gray-700 text-sm w-2/12"
+			)
+			.html("<i class='fas fa-plane-departure'></i>");
+		departTimeEl = $("<div>").addClass("flex flex-col items-center w-10/12");
+		departTimes = $("<p>")
+			.addClass("flex flex-col justify-center w-full")
+			.html(`<p><b>${trip.departLeave}</b> to <b>${trip.departArrive}</b></p>`);
+		departDuration = $("<p>")
+			.addClass("text-gray-700 w-full")
+			.text("5 hr 00 min");
+
+		departTimeEl.append(departTimes, departDuration);
+
+		departEl.append(departIcon, departTimeEl);
+		detailsEl.append(departEl);
+
+		// create a div and children for the return flight times
+		returnEl = $("<div>").addClass("flex flex-row w-full");
+		returnIcon = $("<span>")
+			.addClass(
+				"p-2 flex flex-col justify-center items-center px-3 text-gray-700 text-sm w-2/12"
+			)
+			.html("<i class='fas fa-plane-arrival'></i>");
+		returnTimeEl = $("<div>").addClass("flex flex-col items-center w-10/12");
+		returnTimes = $("<p>")
+			.addClass("flex flex-col justify-center w-full")
+			.html(`<p><b>${trip.returnLeave}</b> to <b>${trip.returnArrive}</b></p>`);
+		returnDuration = $("<p>")
+			.addClass("text-gray-700 w-full")
+			.text("4 hr 30 min");
+
+		returnTimeEl.append(returnTimes, returnDuration);
+
+		returnEl.append(returnIcon, returnTimeEl);
+		detailsEl.append(returnEl);
+
+		// create a div that holds the flight price
+		var priceEl = $("<div>")
+			.addClass(
+				"flex flex-col justify-center items-center font-semibold text-xl text-green-600 w-4/12"
+			)
+			.text(`CA$${trip.price}`);
+
+		// append the detail and price elements to the trip div
+		tripEl.append(detailsEl, priceEl);
+
+		flightsEl.append(tripEl);
+	}
+
+	$("main").prepend(flightsEl);
+
+	// add rotated corners to the bottom-most flight information
+	$(".trip").last().addClass("rounded-b-xl");
+};
+
 // ----- event listeners -----
 
 // listen for the click on the search button
@@ -117,4 +232,7 @@ $("#search-submit").on("click", function (event) {
 
 	// display the destination image and covid information
 	showCOVIDInfo();
+
+	// show flight results based on the search criteria
+	showFlights();
 });
