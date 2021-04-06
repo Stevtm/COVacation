5690c82755ebdd2835e9e276eb423ae213596777
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-04-06 09:10:01 -0400

    removed commented code from script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,6 @@
 $("#search-submit").on("click", showFlightInfo);
 
 // // function to get a list of airports at both origin and destination (separate calls for each)
-// function getPlaces(queryName) {
-//     fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/CA/CAD/en-US/?query=${queryName}`, {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "e94756adcfmsh4384f35646a8fc5p17f530jsn906fc3f8b7a4",
-// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-// 		}
-// 	})
-// 	.then(response => {
-// 		response.json().then(data => {
-// 		console.log(data);
-// 		console.log(data.Places[1].PlaceId); // gets most convenient airport
-
-// 		});
-// 	})
-// 	.catch(err => {
-// 		console.error(err);
-// 	});
-
-// }
-
-// function showFlights() {
-// 	fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/CA/CAD/en-US/YYZ-sky/CLE-sky/anytime?inboundpartialdate=anytime", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "e94756adcfmsh4384f35646a8fc5p17f530jsn906fc3f8b7a4",
-// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-// 		}
-// 	})
-// 	.then(response => {
-// 		console.log(response);
-// 		response.json().then(data => {
-// 			console.log(data);
-// 		})
-// 	})
-// 	.catch(err => {
-// 		console.error(err);
-// 	});
-
-// }
 
 function showFlightInfo() {
 	var departFrom = $("#depart-from").val().trim();
